970d8959f1af3ac9eae4275d0be4265c98d4085c
ahmmadsaleh86 <31177589+ahmmadsaleh86@users.noreply.github.com>
2017-08-20 12:48:21 +0300

    add addon.setup.php
    this file use make the plugin readable by ExpressionEngine

---
+++ addon.setup.php
@@ -0,0 +1,10 @@
+<?php
+
+return array(
+      'author'      => 'Ahmmad Saleh',
+      'author_url'  => 'https://github.com/ahmmadsaleh86',
+      'name'        => 'Json',
+      'description' => 'This plugin will help you in open and read data from json file',
+      'version'     => '1.0.3',
+      'namespace'   => 'json'
+);
